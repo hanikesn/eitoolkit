c1ec1e7d4ae08bc77a3a8284c6eeee7b3c9b15ff
Steffen Hanikel <hanikesn@studi.informatik.uni-stuttgart.de>
2012-06-04 11:18:05 +0200

    Added page to explain thread safety

--- library/include/EIToolkit.h
+++ library/include/EIToolkit.h
@@ -19,6 +19,20 @@
  * There are bindings available for Java and C#, which resemble the C++ API. Therefore this documentation is also valid for them.
  *
  *
+ *
+ * @page threadsafe Reentrancy and Thread-Safety
+ *
+ * Throughout the documentation, the terms @e reentrant and
+ * @e thread-safe are used to mark classes and functions to indicate
+ * how they can be used in multithread applications:
+ *
+ * @li A @e thread-safe function can be called simultaneously from
+ *     multiple threads, even when the invocations use shared data,
+ *     because all references to the shared data are serialized.
+ * @li A @e reentrant function can also be called simultaneously from
+ *     multiple threads, but only if each invocation uses its own data.
+ *
+ * @see http://qt-project.org/doc/threads-reentrancy.html
  */
 
 #include "EIPrerequisites.h"
