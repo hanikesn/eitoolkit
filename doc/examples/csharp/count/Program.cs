702d125a526fa337e022a3168cc0ce2df63fb742
unknown <Steffen@Steffen-Laptop.(none)>
2012-07-04 11:52:36 +0200

    Added csharp example

---
+++ doc/examples/csharp/count/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using de.uni_stuttgart.eitoolkit;
+
+namespace count
+{
+    class Program
+    {
+        static volatile int count = 0;
+
+        class Listener : DataListener
+        {
+            public override void onMessage(DataMessage msg)
+            {
+                String str = msg.getSender();
+                count++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DataListener listener = new Listener();
+            Receiver receiver = new Receiver(new StringMap());
+            receiver.addDataListener(listener);
+
+            System.Console.ReadLine();
+            System.Console.WriteLine(count);
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ doc/examples/csharp/push/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using de.uni_stuttgart.eitoolkit;
+
+namespace push
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sender sender = new Sender(new Description("csharp"),  new StringMap());
+
+            DataMessage msg = new DataMessage("csharp");
+
+            int cnt = 0;
+
+            long start = DateTime.Now.Ticks;
+            while (DateTime.Now.Ticks < start + 5 * TimeSpan.TicksPerSecond)
+            {
+                sender.sendMessage(msg);
+                cnt++;
+            }
+            System.Console.Out.WriteLine(cnt);
+            System.Console.ReadLine();
+        }
+    }
+}
