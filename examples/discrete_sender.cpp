8d59c0e3b648a3fe61e234fbfbb846d6f25c4384
Stefanie Baur <baur.stefanie@gmail.com>
2012-07-04 16:06:42 +0200

    2 weitere diskrete Sender hinzugefügt

---
+++ examples/discrete_sender.cpp
@@ -0,0 +1,28 @@
+#include <EIToolkit.h>
+#include <cmath>
+#include <boost/chrono.hpp>
+#include <boost/thread.hpp>
+
+typedef boost::chrono::high_resolution_clock Clock;
+
+int main()
+{
+    std::string deviceName = "discrete sender";
+
+    EI::Description desc(deviceName, "dummy");
+    std::string name = "regular";
+    desc.addDataSeries(name, EI::DataSeriesInfo(EI::Value::STRING, EI::DataSeriesInfo::NONE, ""));
+
+    EI::Sender sender(desc, EI::StringMap());
+
+    EI::DataMessage p(deviceName);
+
+    Clock::time_point start;
+
+    while(true)
+    {
+        p.setString(name, "ping");
+        sender.sendMessage(p);
+        boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+    }
+}
